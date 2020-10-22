575b9d9fdbf2eb8204a7acdd5351e35795a9ea9f
yodsatorn pantongkam <yodsatornb@gmail.com>
2020-10-23 01:21:01 +0700

    Create Model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from recipes.models import Recipes
 
 # Create your models here.
+
+class Comments(models.Model):
+    userID = models.ManyToManyField(User)
+    recipeID = models.OneToOneField(Recipes,on_delete=models.CASCADE)
+    body = models.CharField(max_length = 4096)
+    voteUpCom = models.ManyToManyField(User)
+    voteDownCom = models.ManyToManyField(User)
+    def __str__(self):
+	    return f"User: {self.userID} | Body: {self.body}"
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Recipes(models.Model):
+    
+    reName = models.CharField(max_length = 128)
+    solution = models.CharField(max_length = 4096)
+    ingredient = models.CharField(max_length = 4096)
+    voteUp = models.ManyToManyField(User)
+    voteDown = models.ManyToManyField(User)
+    def __str__(self):
+	    return f"ID: {self.pk} | Recipe: {self.reName} "
+
+class Tags(models.Model):
+    tagName = models.CharField(max_length = 64)
+    tagID = models.ManyToManyField(Recipes)
+    def __str__(self):
+        return f"Tag = {self.tagName}"
