3b2f2f0b2176f5d2f0ec2827c48d4b8f876f911f
Poonnatuch Boonyarattanasoontorn <6110613228@tu>
2020-10-24 02:03:01 +0700

    fix spaces between command

--- comments/models.py
+++ comments/models.py
@@ -5,10 +5,12 @@ from recipes.models import Recipes
 # Create your models here.
 
 class Comments(models.Model):
+    
     userID = models.ManyToManyField(User)
     recipeID = models.OneToOneField(Recipes,on_delete=models.CASCADE)
     body = models.CharField(max_length = 4096)
     voteUpCom = models.ManyToManyField(User)
     voteDownCom = models.ManyToManyField(User)
+    
     def __str__(self):
 	    return f"User: {self.userID} | Body: {self.body}"
--- recipes/models.py
+++ recipes/models.py
@@ -10,11 +10,13 @@ class Recipes(models.Model):
     ingredient = models.CharField(max_length = 4096)
     voteUp = models.ManyToManyField(User)
     voteDown = models.ManyToManyField(User)
+
     def __str__(self):
 	    return f"ID: {self.pk} | Recipe: {self.reName} "
 
 class Tags(models.Model):
     tagName = models.CharField(max_length = 64)
     tagID = models.ManyToManyField(Recipes)
+    
     def __str__(self):
         return f"Tag = {self.tagName}"
