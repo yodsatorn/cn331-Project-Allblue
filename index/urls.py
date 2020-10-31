9e1b047fae56f5d9081f971aa0aaed7a40e191cd
Poonnatuch Boonyarattanasoontorn <6110613228@student.tu.ac.th>
2020-10-31 18:56:09 +0700

    fix views.login to views.view_login to prevent overiding def

--- index/urls.py
+++ index/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	path('about/', views.about, name = "about"),
 	path('menu2/', views.menu2, name = "menu2"),
 	path('menu3/', views.menu3, name = "menu3"),
-	path('login/', views.login, name="login"),
+	path('login/', views.view_login, name="login"),
 	path('register/', views.register, name="register"),
 ]
