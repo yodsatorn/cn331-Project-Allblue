ad6a68fa6a03d4e131cf77e8174f070e2bd651a1
Poonnatuch Boonyarattanasoontorn <6110613228@student.tu.ac.th>
2020-11-01 20:17:35 +0700

    Change admin and user

--- index/tests.py
+++ index/tests.py
@@ -6,20 +6,23 @@ from django.urls import reverse
 class indexTestCase(TestCase):
     
     def setUp(self):
-       
-        #create user and admin 
-        admin_1 = User.objects.create_superuser(username = 'admin',email = 'admin@tse.com',password = '12345')
-        u1 = User.objects.create_user(username="starter",first_name="Yodsatorn",password="12345",email="yod@tse.com")
-    #happy path login page
+        # create admin 
+        admin1 = User.objects.create_superuser(username = 'admin1',email = 'admin@tse.com',password = 'admin1password')
+
+        # create user
+        u1 = User.objects.create_user(username="user1",first_name="user1",password="user1password",email="user1@tse.com")
+    
+    # happy path login page
     def test_login_page(self):
         c = Client()
-        response_1 =  c.login(username="starter",password="12345")
+        response_1 =  c.login(username="user1",password="admin1password")
         response = c.get('/login')
         self.assertEqual(response.status_code,200)
         self.assertTrue(response_1)
+
     def test_not_login_page(self):
         c = Client()
-        response_1 = c.login(username = "starter",password="54321")
+        response_1 = c.login(username = "user1",password="user1password")
         response = c.get('/login')
         self.assertNotEqual(response.status_code,200)
         self.assertFalse(response_1)
