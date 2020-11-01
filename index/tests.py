631f0366a31be998c2facebd0072044745391056
Poonnatuch Boonyarattanasoontorn <6110613228@student.tu.ac.th>
2020-11-01 21:49:03 +0700

    Create test_login_error_message

--- index/tests.py
+++ index/tests.py
@@ -24,6 +24,14 @@ class indexTestCase(TestCase):
     def test_fail_login(self):
         c = Client()
         # response_1 = c.login(username = "user1",password="user1password")
-        response = c.post('/login',username = "user1", password="admin1password")
+        response = c.post('/login/',username = "user1", password="admin1password")
         self.assertNotEqual(response.status_code,200)
         self.assertFalse(response)
+
+    def test_login_error_message(self):
+        """
+        Test error_message for login page if authenticate() return False (User not in database) error_message be sent to inform Client.
+        """
+        c = Client()
+        response = c.post('/login/', {"username":"wronguser","password":"withworngpassword"}, follow=True)
+        self.assertTrue(response.context['message'] == 'Invalid Credentials')
