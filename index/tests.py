04db2e57029794a0e958af87e5f6e8c8a0b3ec44
yodsatorn pantongkam <yodsatornb@gmail.com>
2020-11-01 21:43:01 +0700

    Test log in success and fail #1

--- index/tests.py
+++ index/tests.py
@@ -1,29 +1,44 @@
-from django.test import Client,TestCase
+from django.test import Client, TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.auth import authenticate
 
 # Create your tests here.
 class indexTestCase(TestCase):
-    
     def setUp(self):
-        # create admin 
-        admin1 = User.objects.create_superuser(username = 'admin1',email = 'admin@tse.com',password = 'admin1password')
+        # create admin
+        admin1 = User.objects.create_superuser(
+            username="admin1", email="admin@tse.com", password="admin1password"
+        )
 
         # create user
-        u1 = User.objects.create_user(username="user1",first_name="user1",password="user1password",email="user1@tse.com")
-    
+        u1 = User.objects.create_user(
+            username="user1",
+            first_name="user1",
+            password="user1password",
+            email="user1@tse.com",
+        )
+
     # happy path login page
     def test_success_login(self):
         c = Client()
-        # response_1 =  c.login(username="user1",password="admin1password")
-        response = c.post('/login/',{"username":"user1","password":"user1password"}, follow=True)
-        self.assertEqual(response.status_code,200)
+        response = c.post(
+            "/login/", {"username": "user1", "password": "user1password"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(response)
+        # check authenticate
+        user = authenticate(username="user1", password="user1password")
+        self.assertTrue((user is not None) and user.is_authenticated)
 
     # sad path login page
     def test_fail_login(self):
         c = Client()
-        # response_1 = c.login(username = "user1",password="user1password")
-        response = c.post('/login',username = "user1", password="admin1password")
-        self.assertNotEqual(response.status_code,200)
-        self.assertFalse(response)
+        response = c.post(
+            "/login", {"username": "user1", "password": "admin1password"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        # TO DO List ^^^^^ check website login fail
+        # check authenticate
+        user = authenticate(username="user1", password="admin1password")
+        self.assertFalse(user is not None and user.is_authenticated)
