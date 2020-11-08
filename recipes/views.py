050b9e43cbdfdfe5a322cf6c6b2f654f6e5278ca
Poonnatuch B <6110613228@student.tu.ac.th>
2020-11-08 14:56:07 +0700

    Create addrecipe_view

--- recipes/views.py
+++ recipes/views.py
@@ -1,38 +1,22 @@
 from django.http import request
 from django.shortcuts import render
 from recipes.models import Recipes,Tags
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .forms import AddRecipe
+from .forms import RecipeForm
 
 def addrecipe_view(request):
     if request.method == "POST" :
-        form = AddRecipe(request.POST)
+        form = RecipeForm(request.POST)
         if form.is_valid():
-                name = form.cleaned_data['recipeName']
-                igd = form.cleaned_data['ingredient']
-                sol = request.POST["solutions"]
-                r = Recipes(reName=name,ingredient=igd,solution=sol)
-                r.save()
-                r.user.add(User.objects.get(id=request.user.id))
-                return HttpResponseRedirect('')
-    else :
-        form = AddRecipe()
+            form.save()
+            form = RecipeForm()
+            return render(request, 'addrecipe.html', {'form' : form})
+    else:
+        form = RecipeForm(initial={'user': [request.user] })
 
-    return render(request, 'AddRecipe.html', {'form': form})
-
-# add recipe
-def addRecipe(request):
-    if request.method == "POST":
-        name= request.POST["recipes_Name"]
-        igd = request.POST["ingredient"]
-        sol = request.POST["solutions"]
-        r = Recipes(reName=name,ingredient=igd,solution=sol)
-        r.save()
-        r.user.add(User.objects.get(id=request.user.id))
-        
-    return render( )
+    return render(request, 'addrecipe.html', {'form' : form})
 
 def deleteRecipe(request ,recipe_id):
     if request.method == "POST":
