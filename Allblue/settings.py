628d4755deb13cccb1721c9f4164aa3cc980b414
pakcawat issarawisarnpol <paradox_32@hotmail.co.th>
2020-11-14 06:05:03 +0000

    addimage_addcomment

--- Allblue/settings.py
+++ Allblue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,5 +133,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 
--- recipes/tests.py
+++ recipes/tests.py
@@ -5,16 +5,18 @@ from django.urls import reverse
 from django.contrib.auth import authenticate
 from django.db import IntegrityError
 from .models import Recipes
+from django.core.files import File
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 # Create your tests here.
 class RecipesTestCase(TestCase):
-    
+
     def setUp(self):
         # create user
         u1 = User.objects.create_user(username="user1",first_name="user1",password="user1password",email="user1@tse.com",)
         #crete recipes
         r = Recipes.objects.create(reName='rice omlet',ingredient='rice egg',solution='sol')
-         
+
     def test_valid_recipes(self):
         """Check that Recipes is valid"""
         c = Recipes.objects.filter(pk = 1).exists()
@@ -24,3 +26,11 @@ class RecipesTestCase(TestCase):
         """Check that Recipes is invalid"""
         c = Recipes.objects.filter(pk = 4).exists()
         self.assertFalse(c)
+
+    def test_image(self):
+        m1 = Recipes()
+        m1.image = SimpleUploadedFile(name='test_image.jpg', content=open("static/menu/images/kapao.jpg", 'rb').read(), content_type='image/jpeg')
+        user1=Recipes(reName='abc')
+        user1.image.save('abc.png', File(open('static/menu/images/pancake.jpg', 'rb')))
+        user1.save()
+        self.assertEqual(Recipes.objects.count(), 2)
