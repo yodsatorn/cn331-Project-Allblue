085cf048efd9e6ac0c6cb19c99e815593d57c0cb
yodsatorn pantongkam <yodsatornb@gmail.com>
2020-11-21 15:28:07 +0700

    update test

--- comments/views.py
+++ comments/views.py
@@ -8,22 +8,9 @@ from recipes.models import Recipes
 from django.shortcuts import redirect
 # Create your views here.
 
-# This fucntion will add comment that writen by user.
 
-
-def addcomments(request, recipes_id):
-    if request.method == "POST":
-        user = User.objects.get(id=request.user.id)
-        recipe = Recipes.objects.get(pk=recipes_id)
-        body = request.POST['body']
-        comment = Comments.objects.create(body=body)
-        comment.userID.add(user)
-        comment.recipeID.add(recipe)
-        return redirect('recipe_view', recipes_id)
 
 # Vote up comment
-
-
 def voteUp(request, recipe_id, id):
     comments = Comments.objects.get(id=id)
     user = User.objects.get(id=request.user.id)
